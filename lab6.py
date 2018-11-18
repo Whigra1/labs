d577c5079923d7c73fb22283ef56dd084852ded6
Whigra1 <43039301+Whigra1@users.noreply.github.com>
2018-11-18 18:41:30 +0200

    Add files via upload

---
+++ lab6.py
@@ -0,0 +1,23 @@
+def func(n):
+    res = []
+    for i in range(1,n):
+        if n % i == 0:
+            if simple(i):
+                res.append(i)
+    return res
+
+
+def simple(i):
+    if i < 10:
+        if i == 3 or i == 2 or i == 5 or i == 7:
+            return True
+    else:
+        if i % 2 != 0 and i % 3 != 0 and i % 5 != 0:
+            return True
+    return False
+
+
+if __name__ == '__main__':
+    for i in range(101):
+        print(i,func(i))
+    
