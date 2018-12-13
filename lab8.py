56931334d1200bb1d6f18ddbd271a9f6d77afa9d
Whigra1 <43039301+Whigra1@users.noreply.github.com>
2018-12-13 20:46:12 +0200

    Update lab8.py

--- lab8.py
+++ lab8.py
@@ -9,31 +9,37 @@ import random
 def createMatrix(i, j):
     return [[random.randrange(10) for _ in range(j)] for _ in range(i)]
 
-
-def main(m, n):
-    matrix = createMatrix(m, n)
-
-    for row in matrix:
-        row.append(0)
-
-    for ind, row in enumerate(matrix):
-        matrix[ind][-1] = sum(row)
-
+def column_sum(matrix):
     temp = []
-
     for column in range(len(matrix)):
         res = 0
         for row in matrix:
             res += row[column]
         temp.append(res)
-    matrix.append(temp)
-    res = 0
+    return temp
 
+
+def row_sum(matrix):
+    for ind, row in enumerate(matrix):
+        matrix[ind][-1] = sum(row)
+        
+
+def matrix_sum(matrix):
+    res = 0
     for row in matrix:
         for num in row:
            res += num
+    return res
 
-    matrix[m][n] = res
+def main(m, n):
+    matrix = createMatrix(m, n)
+    for row in matrix:
+        row.append(0)
+        
+    row_sum(matrix)
+    row_tith_sums = column_sum(matrix)
+    matrix.append(row_tith_sums)
+    matrix[m][n] = matrix_sum(matrix)
     return matrix
 
 
