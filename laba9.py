b7028a7a6f2486bf75da97201c55290fb2b338f5
Whigra1 <43039301+Whigra1@users.noreply.github.com>
2018-12-13 20:52:09 +0200

    Update laba9.py

--- laba9.py
+++ laba9.py
@@ -2,23 +2,26 @@
 довжина яких більша за вказану користувачем."""
 
 
+def clear(word):
+    separators = ',:.-,\''
+    if word.startswith(tuple(separators)): # Удаление лишних символов (',', ':', '.', '-', '\'')
+        word = word[1:]                       # C начала строки 
+    elif word.endswith(tuple(separators)):
+        word = word[0:-1]                   # Удаление лишних символов (',', ':', '.', '-', '\'')
+    return word                                  # C конца строки
+
+
 def word_counter(string, n):
     separators = ',:.-'
     for i in string:
         if i in separators:
             string.replace(i,' ')
-    separators += "'"
     word_list = []
-    for i in string.split():
-        if len(i) < n:            
-            if i.startswith(tuple(separators)): # Удаление лишних символов (',', ':', '.', '-', '\'')
-                i = i[1:]                       # C начала строки 
-            elif i.endswith(tuple(separators)):
-                i = i[0:-1]                   # Удаление лишних символов (',', ':', '.', '-', '\'')
-            word_list.append(i)               # C конца строки
+    for word in string.split():
+        if len(clear(word)) < n:                                          
+            word_list.append(word)               
     return len(word_list), word_list
-
-
+            
 if __name__ == '__main__':
     d = word_counter("John's mom went there, but he wasn't there. So she said: 'Where are you'",5)
     print(d)
